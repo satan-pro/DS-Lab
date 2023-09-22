000f478b3948ce8b67d7a2b0ddbee8fcbb608b82
satan-pro <sayantancreator@gmail.com>
2023-09-22 08:24:51 +0530

    Lab5 prg4 added

--- Lab 5/prg4.c
+++ Lab 5/prg4.c
@@ -1,72 +1,65 @@
+/* Given an array arr with n elements and a number k, k<n. The task is to delete k
+elements which are smaller than next element (i.e., we delete arr[i] if arr[i] <
+arr[i+1]) or become smaller than next because next element is deleted. Example:
+Input: arr[] = {20, 10, 25, 30, 40}, k = 2
+Output: 25 30 40
+Explanation: First we delete 10 because it follows arr[i] < arr[i+1]. Then we delete 20
+because 25 is moved next to it and it also starts following the condition. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 5;
-
-struct arr{
-    int size;
-    int arr[];
-};
-
-/* void push(struct arr* a, int d)
+int delete(int d, int size, int arr[])
 {
-    if(a->top<(a->size-1))
+    for(int i=d; i<size-1; i++)
     {
-        a->arr[++a->top]=d;
+        arr[i]=arr[i+1];
     }
-} */
-
-void delete(int d, struct arr* a)
-{
-    for(int i=d; i<a->size-1; i++)
-    {
-        a->arr[i]=a->arr[i+1];
-    }
-    a->size--;
+    size--;
+    return size;
 }
 
-void task(struct arr* a, int n)
+int task(int size, int n, int arr[])
 {
     for(int i=1; i<=n; i++)
     {
-        for(int j=0; j<a->size; j++)
+        int j=0;
+        int flag=0;
+        while(j<size-1 && flag==0)
         {
-            if(a->arr[j]<a->arr[j+1])
+            if(arr[j]<arr[j+1])
             {
-                delete(j,a);
+                size = delete(j,size,arr);
+                flag=1;
             }
+            j++;
         }
     }
+    return size;
 }
 
-void print(struct arr* a)
+void print(int size, int arr[])
 {
-    for(int i=0; i<a->size; i++)
+    for(int i=0; i<size; i++)
     {
-        printf("%d ",a->arr[i]);
+        printf("%d ",arr[i]);
     }
 }
 
 int main()
 {
-    struct arr a;
     int n,k;
     printf("Enter size of array\n");
     scanf("%d", &n);
-    a.size = n;
-    a.arr[a.size];
-    /* a.arr[a.size]={20,10,25,30,40}; */
-    //a.top=-1;
-    /* printf("%d",a.size);*/
-    for(int i=0; i<a.size; i++)
+    int arr[n];
+    for(int i=0; i<n; i++)
     {
         printf("Enter array elements\n");
-        scanf("%d",&n);
-        a.arr[i]=n;
+        scanf("%d",&arr[i]);
     } 
     
     printf("Enter k\n");
     scanf("%d", &k);
-    task(&a,k);
-    print(&a);
+    n = task(n,k,arr);
+    print(n,arr);
 }
